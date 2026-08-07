cae94897adaa00a644f60d8925e72475aa38d9db
Jenushan <jenushan349@gmail.com>
2026-08-07 19:10:30 -0400

    feat: add lambda error handling

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,35 +6,43 @@ s3_client = boto3.client("s3")
 bedrock_client = boto3.client("bedrock-runtime",region_name="us-east-1")
 
 def classify_tweet(tweet): 
+  max_attempts = 3
+
   prompt = f"""
   You are classifying the suicide related risk in a tweet. Classify the tweet into one of these four categories: high risk, potentially likely, neutral, or unlikely. Return only the category and nothing else.
 
   Tweet: {tweet}
   """
 
-  response = bedrock_client.converse(
-    modelId="amazon.nova-micro-v1:0",
-    messages = [{ "role": "user", "content": [{"text": prompt}]}]
-  )
+  for attempt in range(max_attempts):
+    try:
+      response = bedrock_client.converse(modelId="amazon.nova-micro-v1:0", messages = [{ "role": "user", "content": [{"text": prompt}]}])
 
-  output = response['output']['message']['content'][0]['text'].lower().strip()
+      output = response['output']['message']['content'][0]['text'].lower().strip()
 
-  valid_output = False 
-  allowed_values = ["high risk", "potentially likely", "neutral", "unlikely"]
-  alert_of_risk = None
+      allowed_values = ["high risk", "potentially likely", "neutral", "unlikely"]
+      alert_of_risk = None
 
-  if output in allowed_values: 
-    valid_output = True 
+      if output not in allowed_values: 
+        return None, None 
+   
+      if output == "high risk" or output == "potentially likely": 
+        alert_of_risk = 1
+      elif output == "neutral" or output == "unlikely": 
+        alert_of_risk = 0
+      else: 
+        alert_of_risk = None
 
-  if output == "high risk" or output == "potentially likely": 
-    alert_of_risk = 1
-  elif output == "neutral" or output == "unlikely": 
-    alert_of_risk = 0
-  else: 
-    alert_of_risk = None
+      return alert_of_risk, output
 
-  return valid_output, alert_of_risk, output
+    except Exception as error:
+      print(f"Attempt {attempt + 1} failed: {error}")
 
+      # Prevents program from waiting after final failed attempt
+      if attempt < max_attempts - 1: 
+        time.sleep(0.1)
+
+  return None, None
 
 def lambda_handler(event, context):
   bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
@@ -60,12 +68,12 @@ def lambda_handler(event, context):
   alerts = []
 
   for tweet in sample_df["Tweet"]: 
-    tweet_valid_output, tweet_alert_of_risk, tweet_output = classify_tweet(tweet)
+    tweet_alert_of_risk, tweet_output = classify_tweet(tweet)
 
     classifications.append(tweet_output)
     alerts.append(tweet_alert_of_risk)
 
-    print(tweet_valid_output, tweet_alert_of_risk, tweet_output)
+    print(tweet_alert_of_risk, tweet_output)
     time.sleep(0.1)
 
   sample_df['suicide_likelihood'] = classifications 
